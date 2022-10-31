8a0604409e9f1f064271604cc8580fa8988ea29f
Xie Loong <56944321+xieloong@users.noreply.github.com>
2022-10-31 22:43:11 +0800

    INPUT SYSTEM LOCAL PLAY

---
+++ 190874H_IWP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    private Vector2 movementInput;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(movementInput.x, movementInput.y) * speed * Time.deltaTime);
+    }
+
+    public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();  
+}
