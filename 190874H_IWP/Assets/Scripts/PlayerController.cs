3f5488861a951e238790492b6ac54e359fdf2c7c
Xie Loong <56944321+xieloong@users.noreply.github.com>
2022-11-06 00:21:31 +0800

    clamp environment + input system on shooting setup

--- 190874H_IWP/Assets/Scripts/PlayerController.cs
+++ 190874H_IWP/Assets/Scripts/PlayerController.cs
@@ -6,12 +6,45 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5;
-    private Vector2 movementInput;
+    private Vector2 movementInput = Vector2.zero;
+    private Controls controls;
+    Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Awake()
+    {
+        controls = new Controls();
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(new Vector2(movementInput.x, movementInput.y) * speed * Time.deltaTime);
+        
     }
 
-    public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+
+        movementInput = ctx.ReadValue<Vector2>();
+    }
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire!");
+    }
 }
