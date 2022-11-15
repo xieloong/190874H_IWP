d0e7b15bf0f051b3319a02e58dcc967d10854f37
Xie Loong <56944321+xieloong@users.noreply.github.com>
2022-11-15 22:24:04 +0800

    new enemy added (red plane thingy)

--- 190874H_IWP/Assets/Scripts/BulletScript.cs
+++ 190874H_IWP/Assets/Scripts/BulletScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     public float bulletSpeed = 10f;
-    public float bulletDamageTaken = 10f;
+    public float bulletDamageTaken = 1f;
     // Start is called before the first frame update
     void Start()
     {
